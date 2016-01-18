lwip-tcpip/lwip

commit f2c7e9c939ed612172b0e026aad12b1652d4ca17

raw: Remove unnecessary #if guard around PCB_ISIPV6() calls
PCB_ISIPV6() macro is well defined for all cases (LWIP_IPV4 && LWIP_IPV6,
LWIP_IPV4 only and LWIP_IPV6 only), thus remove the unnecessary #if guard
around PCB_ISIPV6() calls.

Signed-off-by: Axel Lin <axel.lin@ingics.com>

src/core/raw.c
@@ -112,9 +112,7 @@ raw_input(struct pbuf *p, struct netif *inp)
 #if IP_SOF_BROADCAST_RECV
       /* broadcast filter? */
       if ((ip_get_option(pcb, SOF_BROADCAST) || !ip_addr_isbroadcast(ip_current_dest_addr(), ip_current_netif()))
-#if LWIP_IPV6
           || PCB_ISIPV6(pcb)
-#endif /* LWIP_IPV6 */
           )
 #endif /* IP_SOF_BROADCAST_RECV */
       {
@@ -248,14 +246,7 @@ raw_sendto(struct raw_pcb *pcb, struct pbuf *p, const ip_addr_t *ipaddr)
 
   LWIP_DEBUGF(RAW_DEBUG | LWIP_DBG_TRACE, ("raw_sendto\n"));
 
-  header_size = (
-#if LWIP_IPV4 && LWIP_IPV6
-    PCB_ISIPV6(pcb) ? IP6_HLEN : IP_HLEN);
-#elif LWIP_IPV4
-    IP_HLEN);
-#else
-    IP6_HLEN);
-#endif
+  header_size = PCB_ISIPV6(pcb) ? IP6_HLEN : IP_HLEN;
 
   /* not enough space to add an IP header to first pbuf in given p chain? */
   if (pbuf_header(p, header_size)) {
@@ -293,9 +284,7 @@ raw_sendto(struct raw_pcb *pcb, struct pbuf *p, const ip_addr_t *ipaddr)
   }
 
 #if IP_SOF_BROADCAST
-#if LWIP_IPV6
   if (!PCB_ISIPV6(pcb))
-#endif /* LWIP_IPV6 */
   {
     /* broadcast filter? */
     if (!ip_get_option(pcb, SOF_BROADCAST) && ip_addr_isbroadcast(ipaddr, netif)) {

cagstech/lwip-ce

commit 0c6aa79c8e1927aea626e2582c57fcf1091d2a15

raw: Fix build error
IP6_HLEN is only defined when LWIP_IPV6, IP_HLEN is only defined when LWIP_IPV4.
This fixes build error in !LWIP_IPV4 || !LWIP_IPV6 cases.

Fixes: f2c7e9c939ed ("raw: Remove unnecessary #if guard around PCB_ISIPV6() calls")
Reported-by: Erik Ekman
Signed-off-by: Axel Lin <axel.lin@ingics.com>

src/core/raw.c
@@ -246,7 +246,14 @@ raw_sendto(struct raw_pcb *pcb, struct pbuf *p, const ip_addr_t *ipaddr)
 
   LWIP_DEBUGF(RAW_DEBUG | LWIP_DBG_TRACE, ("raw_sendto\n"));
 
-  header_size = PCB_ISIPV6(pcb) ? IP6_HLEN : IP_HLEN;
+  header_size = (
+#if LWIP_IPV4 && LWIP_IPV6
+    PCB_ISIPV6(pcb) ? IP6_HLEN : IP_HLEN);
+#elif LWIP_IPV4
+    IP_HLEN);
+#else
+    IP6_HLEN);
+#endif
 
   /* not enough space to add an IP header to first pbuf in given p chain? */
   if (pbuf_header(p, header_size)) {