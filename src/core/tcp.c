lwip-tcpip/lwip

commit 44e1a2d8e23fee50adf4f14bd7a82b87e3a14e2a

define tcp_backlog_set() as dummy-define when backlog feature is disable

src/core/tcp.c
@@ -586,7 +586,7 @@ tcp_listen_with_backlog(struct tcp_pcb *pcb, u8_t backlog)
 #endif /* LWIP_CALLBACK_API */
 #if TCP_LISTEN_BACKLOG
   lpcb->accepts_pending = 0;
-  tcp_backlog_set(lpcb, backlog);
+  lpcb->backlog = backlog;
 #endif /* TCP_LISTEN_BACKLOG */
   TCP_REG(&tcp_listen_pcbs.pcbs, (struct tcp_pcb *)lpcb);
   return (struct tcp_pcb *)lpcb;

src/api/api_msg.c
@@ -1350,9 +1350,7 @@ lwip_netconn_do_listen(void *m)
         } else if (msg->conn->state == NETCONN_LISTEN) {
           /* already listening, allow updating of the backlog */
           msg->err = ERR_OK;
-#if TCP_LISTEN_BACKLOG
           tcp_backlog_set(msg->conn->pcb.tcp, msg->msg.lb.backlog);
-#endif /* TCP_LISTEN_BACKLOG */
         }
       } else {
         msg->err = ERR_ARG;

src/include/lwip/tcp.h
@@ -366,6 +366,7 @@ void             tcp_err     (struct tcp_pcb *pcb, tcp_err_fn err);
 #else  /* TCP_LISTEN_BACKLOG */
 #define          tcp_accepted(pcb) LWIP_ASSERT("pcb->state == LISTEN (called for wrong pcb?)", \
                                                (pcb)->state == LISTEN)
+#define          tcp_backlog_set(pcb, new_backlog)
 #endif /* TCP_LISTEN_BACKLOG */
 
 void             tcp_recved  (struct tcp_pcb *pcb, u16_t len);

cagstech/lwip-ce

commit 27643bda8dec2402b5bfa2289a648bf90e3112c1

tcp: Prevents a 0 sized (invalid) backlog
commit 44e1a2d8e23f accidently includes below changes in tcp_listen_with_backlog
 -  tcp_backlog_set(lpcb, backlog);
 +  lpcb->backlog = backlog;

Thus pass 0 to the backlog parameter of netconn_listen_with_backlog() fails.

Fixes: 44e1a2d8e23f ("define tcp_backlog_set() as dummy-define when backlog feature is disable")
Signed-off-by: Axel Lin <axel.lin@ingics.com>
Signed-off-by: goldsimon <goldsimon@gmx.de>

src/core/tcp.c
@@ -688,7 +688,7 @@ tcp_listen_with_backlog(struct tcp_pcb *pcb, u8_t backlog)
 #endif /* LWIP_CALLBACK_API */
 #if TCP_LISTEN_BACKLOG
   lpcb->accepts_pending = 0;
-  lpcb->backlog = backlog;
+  tcp_backlog_set(lpcb, backlog);
 #endif /* TCP_LISTEN_BACKLOG */
   TCP_REG(&tcp_listen_pcbs.pcbs, (struct tcp_pcb *)lpcb);
   return (struct tcp_pcb *)lpcb;