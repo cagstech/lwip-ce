lwip-tcpip/lwip

commit 5c1dd6a4c65b9fb9c8628ffc911478855979d6e9

Optimization in igmp_remove_group() pointed out by Axel Lin No need to handle special case "first in list" since this is always the allsystems group that shall not be removed

src/core/ipv4/igmp.c
@@ -295,23 +295,23 @@ static err_t
 igmp_remove_group(struct netif* netif, struct igmp_group *group)
 {
   err_t err = ERR_OK;
+  struct igmp_group *tmp_group = netif_igmp_data(netif);
 
-  /* Is it the first group? */
-  if (netif_igmp_data(netif) == group) {
-    netif_set_client_data(netif, LWIP_NETIF_CLIENT_DATA_INDEX_IGMP, group->next);
-  } else {
-    /* look for group further down the list */
-    struct igmp_group *tmpGroup;
-    for (tmpGroup = netif_igmp_data(netif); tmpGroup != NULL; tmpGroup = tmpGroup->next) {
-      if (tmpGroup->next == group) {
-        tmpGroup->next = group->next;
-        break;
-      }
+  /* Skip the first group in the list, it is always the allsystems group added in igmp_start() */
+  if(tmp_group != NULL) {
+    tmp_group = tmp_group->next;
+  }
+
+  /* look for group further down the list */
+  for (; tmp_group != NULL; tmp_group = tmp_group->next) {
+    if (tmp_group->next == group) {
+      tmp_group->next = group->next;
+      break;
     }
-    /* Group not found in the global igmp_group_list */
-    if (tmpGroup == NULL) {
-      err = ERR_ARG;
-    }
+  }
+  /* Group not found in the global igmp_group_list */
+  if (tmp_group == NULL) {
+    err = ERR_ARG;
   }
 
   return err;

cagstech/lwip-ce

commit d42bffa6b5b90e56ecbb11922879d7c5a6513bd6

igmp: Fix optimized code for igmp_remove_group
The code in the for loop checks tmp_group->next == group, so current code
actually checks from the 3rd entry in the linked groups list. Fix it.

Fixes: 5c1dd6a4c65b ("Optimization in igmp_remove_group() pointed out by Axel Lin")
Signed-off-by: Axel Lin <axel.lin@ingics.com>

src/core/ipv4/igmp.c
@@ -295,15 +295,10 @@ static err_t
 igmp_remove_group(struct netif* netif, struct igmp_group *group)
 {
   err_t err = ERR_OK;
-  struct igmp_group *tmp_group = netif_igmp_data(netif);
+  struct igmp_group *tmp_group;
 
   /* Skip the first group in the list, it is always the allsystems group added in igmp_start() */
-  if(tmp_group != NULL) {
-    tmp_group = tmp_group->next;
-  }
-
-  /* look for group further down the list */
-  for (; tmp_group != NULL; tmp_group = tmp_group->next) {
+  for (tmp_group = netif_igmp_data(netif); tmp_group != NULL; tmp_group = tmp_group->next) {
     if (tmp_group->next == group) {
       tmp_group->next = group->next;
       break;