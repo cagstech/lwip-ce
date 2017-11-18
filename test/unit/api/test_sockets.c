lwip-tcpip/lwip

commit b71d4477eab7c19c34e57e5c9a1f8add63de7879

socket tcp performance tweak: handle window update (call into tcpipi_thread) only once per recv for multi-segment receives

src/api/sockets.c
@@ -834,7 +834,7 @@ lwip_listen(int s, int backlog)
 static int
 lwip_recv_tcp(struct lwip_sock *sock, void *mem, size_t len, int flags)
 {
-  u8_t apiflags;
+  u8_t apiflags = NETCONN_NOAUTORCVD;
   int recvd = 0;
   int recv_left = len;
 
@@ -873,8 +873,7 @@ lwip_recv_tcp(struct lwip_sock *sock, void *mem, size_t len, int flags)
               sock->conn->callback(sock->conn, NETCONN_EVT_RCVPLUS, 0);
             }
           }
-          sock_set_errno(sock, 0);
-          return recvd;
+          goto lwip_recv_tcp_done;
         }
         /* We should really do some error checking here. */
         LWIP_DEBUGF(SOCKETS_DEBUG, ("lwip_recv_tcp: p == NULL, error is \"%s\"!\n",
@@ -928,6 +927,12 @@ lwip_recv_tcp(struct lwip_sock *sock, void *mem, size_t len, int flags)
     apiflags |= NETCONN_DONTBLOCK;
     /* @todo: do we need to support peeking more than one pbuf? */
   } while ((recv_left > 0) || (flags & MSG_PEEK));
+lwip_recv_tcp_done:
+  if (recvd > 0) {
+    /* ensure window update after copying all data */
+    netconn_tcp_recvd(sock->conn, recvd);
+  }
+  sock_set_errno(sock, 0);
   return recvd;
 }
 

cagstech/lwip-ce

commit ce5b6c3195ad249d2d52b0a4ccaac8692fd5a90b

Fix broken MSG_PEEK on TCP sockets (post-2.0.3 bug)
MSG_PEEK on TCP sockets was broken since commit b71d4477eab7c19c34e57e5c9a1f8add63de7879
from 06.03.2017: recv hung in an endless loop and tcp_recved() was called for peeked data
(which would result in a too large window advertised).

Aded TCP MSG_PEEK to socket unit tests

src/api/sockets.c
@@ -926,9 +926,9 @@ lwip_recv_tcp(struct lwip_sock *sock, void *mem, size_t len, int flags)
     /* once we have some data to return, only add more if we don't need to wait */
     apiflags |= NETCONN_DONTBLOCK;
     /* @todo: do we need to support peeking more than one pbuf? */
-  } while ((recv_left > 0) || (flags & MSG_PEEK));
+  } while ((recv_left > 0) && !(flags & MSG_PEEK));
 lwip_recv_tcp_done:
-  if (recvd > 0) {
+  if ((recvd > 0) && !(flags & MSG_PEEK)) {
     /* ensure window update after copying all data */
     netconn_tcp_recvd(sock->conn, (size_t)recvd);
   }

test/unit/api/test_sockets.c
@@ -161,6 +161,12 @@ static void test_sockets_allfunctions_basic_domain(int domain)
 
   while(tcpip_thread_poll_one());
 
+  ret = lwip_recv(s2, buf, 3, MSG_PEEK);
+  fail_unless(ret == 3);
+
+  ret = lwip_recv(s2, buf, 3, MSG_PEEK);
+  fail_unless(ret == 3);
+
   ret = lwip_read(s2, buf, 4);
   fail_unless(ret == 4);
 