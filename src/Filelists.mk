cagstech/lwip-ce

commit cab1a33d87c1167b559b41ea3f571e46f3e3779e

add new http files to Filelists.mk

src/Filelists.mk
@@ -154,9 +154,11 @@ SNMPFILES=$(LWIPDIR)/apps/snmp/snmp_asn1.c \
 	$(LWIPDIR)/apps/snmp/snmp_threadsync.c \
 	$(LWIPDIR)/apps/snmp/snmp_traps.c
 
-# HTTPDFILES: HTTP server
-HTTPDFILES=$(LWIPDIR)/apps/http/fs.c \
-	$(LWIPDIR)/apps/http/httpd.c
+# HTTPFILES: HTTP server + client
+HTTPFILES=$(LWIPDIR)/apps/http/altcp_proxyconnect.c \
+    $(LWIPDIR)/apps/http/fs.c \
+	$(LWIPDIR)/apps/http/http_client.c \
+	$(LWIPDIR)/apps/http/httpd.c \
 
 # MAKEFSDATA: MAKEFSDATA HTTP server host utility
 MAKEFSDATAFILES=$(LWIPDIR)/apps/http/makefsdata/makefsdata.c

commit 7a71a47d602d81dc76b674666c4425648b4c80b2

Fix build error for http files
The HTTPDFILES is renamed to HTTPFILES now.

Fixes: cab1a33d87c1 ("add new http files to Filelists.mk")
Signed-off-by: Axel Lin <axel.lin@ingics.com>

src/Filelists.mk
@@ -156,9 +156,9 @@ SNMPFILES=$(LWIPDIR)/apps/snmp/snmp_asn1.c \
 
 # HTTPFILES: HTTP server + client
 HTTPFILES=$(LWIPDIR)/apps/http/altcp_proxyconnect.c \
-    $(LWIPDIR)/apps/http/fs.c \
+	$(LWIPDIR)/apps/http/fs.c \
 	$(LWIPDIR)/apps/http/http_client.c \
-	$(LWIPDIR)/apps/http/httpd.c \
+	$(LWIPDIR)/apps/http/httpd.c
 
 # MAKEFSDATA: MAKEFSDATA HTTP server host utility
 MAKEFSDATAFILES=$(LWIPDIR)/apps/http/makefsdata/makefsdata.c
@@ -191,7 +191,7 @@ MBEDTLS_FILES=$(LWIPDIR)/apps/altcp_tls/altcp_tls_mbedtls.c \
 
 # LWIPAPPFILES: All LWIP APPs
 LWIPAPPFILES=$(SNMPFILES) \
-	$(HTTPDFILES) \
+	$(HTTPFILES) \
 	$(LWIPERFFILES) \
 	$(SMTPFILES) \
 	$(SNTPFILES) \