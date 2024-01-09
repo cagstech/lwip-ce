bd6247308a005be13b66ab633f0881cf406510b1
Anthony Cagliano <acagliano97@gmail.com>
2024-01-09 14:09:31 -0500

    improvements and completions

--- src/ecm.h
+++ src/ecm.h
@@ -18,25 +18,28 @@
 typedef enum
 {
   ECM_OK,
-  ECM_USB_INIT_FAIL,
-  ECM_DEVICE_UNSUPPORTED,
-  ECM_MEMORY_OVERFLOW,
-  ECM_DEVICE_CONFIG_ERROR,
-
+  ECM_ERROR_NO_DEVICE = -1,
+  ECM_ERROR_INVALID_DEVICE = -2,
+  ECM_ERROR_MEMORY = -3,
+  ECM_ERROR_INVALID_DESCRIPTOR = -4,
+  ECM_CONFIGURATION_ERROR = -5,
+  ECM_INTERFACE_ERROR = -6,
+  ECM_ERROR_TX = -7,
+  ECM_ERROR_RX = -8
 } ecm_error_t;
 
-struct ecm_state
+typedef struct _ecm_device_t
 {
   usb_device_t usb_device;
-  usb_interface_descriptor_t if_control, if_data;
-  usb_descriptor_t configuration;
-  usb_endpoint_t ecm_in, ecm_out;
-};
-extern struct ecm_state ecm_state;
+  usb_interface_descriptor_t *if_control, *if_data;
+  // usb_descriptor_t *conf_active;
+  usb_endpoint_t in, out;
+} ecm_device_t;
+extern ecm_device_t ecm_device;
 
 bool ecm_init(void);
 struct pbuf *ecm_receive(void);
-err_t ecm_transmit(struct netif *netif, struct pbuf *p);
+ecm_erorr_t ecm_transmit(struct netif *netif, struct pbuf *p);
 
 usb_error_t ecm_handle_usb_event(usb_event_t event, void *event_data,
                                  usb_callback_data_t *callback_data)
