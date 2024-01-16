494eae2424360b9bce84093b9a77b24f0c584c0f
Anthony Cagliano <acagliano97@gmail.com>
2024-01-16 13:33:59 -0500

    Revert "push debug statements"
    This reverts commit ac84ee75c0abf042b2cd5a59d70071b06a207b5e.

---
+++ obj/src/core/ipv6/inet6.c.d
@@ -0,0 +1,13 @@
+obj/src/core/ipv6/inet6.c.bc: src/core/ipv6/inet6.c \
+  src/include/lwip/opt.h src/include/lwipopts.h src/include/lwip/debug.h \
+  src/include/lwip/arch.h src/include/arch/cc.h \
+  /home/john/installs/CEdev/include/debug.h \
+  /home/john/installs/CEdev/include/stdio.h \
+  /home/john/installs/CEdev/include/cdefs.h \
+  /home/john/installs/CEdev/include/stdarg.h \
+  /home/john/installs/CEdev/include/stdlib.h \
+  /home/john/installs/CEdev/include/stddef.h \
+  /home/john/installs/CEdev/include/stdint.h \
+  /home/john/installs/CEdev/include/inttypes.h \
+  /home/john/installs/CEdev/include/limits.h \
+  /home/john/installs/CEdev/include/ctype.h
