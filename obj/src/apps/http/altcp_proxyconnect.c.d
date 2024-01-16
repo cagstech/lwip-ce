39f74be897917e9b9ce77d20d640b7679e9910aa
Anthony Cagliano <acagliano97@gmail.com>
2024-01-16 13:33:59 -0500

    Revert "push debug statements"
    This reverts commit fc79506f6e3f76bc4f632b97983c25013d07e677.

---
+++ obj/src/apps/http/altcp_proxyconnect.c.d
@@ -0,0 +1,22 @@
+obj/src/apps/http/altcp_proxyconnect.c.bc: \
+  src/apps/http/altcp_proxyconnect.c \
+  src/include/lwip/apps/altcp_proxyconnect.h src/include/lwip/opt.h \
+  src/include/lwipopts.h src/include/lwip/debug.h \
+  src/include/lwip/arch.h src/include/arch/cc.h \
+  /home/john/installs/CEdev/include/debug.h \
+  /home/john/installs/CEdev/include/stdio.h \
+  /home/john/installs/CEdev/include/cdefs.h \
+  /home/john/installs/CEdev/include/stdarg.h \
+  /home/john/installs/CEdev/include/stdlib.h \
+  /home/john/installs/CEdev/include/stddef.h \
+  /home/john/installs/CEdev/include/stdint.h \
+  /home/john/installs/CEdev/include/inttypes.h \
+  /home/john/installs/CEdev/include/limits.h \
+  /home/john/installs/CEdev/include/ctype.h src/include/lwip/ip_addr.h \
+  src/include/lwip/def.h src/include/lwip/ip4_addr.h \
+  src/include/lwip/ip6_addr.h src/include/lwip/ip6_zone.h \
+  src/include/lwip/altcp.h src/include/lwip/tcpbase.h \
+  src/include/lwip/err.h src/include/lwip/pbuf.h \
+  src/include/lwip/priv/altcp_priv.h src/include/lwip/altcp_tcp.h \
+  src/include/lwip/altcp_tls.h src/include/lwip/mem.h \
+  src/include/lwip/init.h
