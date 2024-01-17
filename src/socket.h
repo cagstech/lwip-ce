9f47d62033d19718b04e305ad790e261054bf69b
Anthony Cagliano <acagliano97@gmail.com>
2024-01-17 14:26:07 -0500

    re-push lwip core

---
+++ src/socket.h
@@ -0,0 +1,15 @@
+#ifndef socket_h
+#define socket_h
+
+#include "ecm.h"
+
+typedef enum _socket_error
+{
+  SOCKET_SUCCESS,
+  SOCKET_ECM_INIT_FAIL,
+} socket_error_t;
+
+struct _socket
+{
+
+} socket;
