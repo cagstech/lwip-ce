ce48298d87fefc5b9906136675c4f5c3002f1899
Anthony Cagliano <acagliano97@gmail.com>
2024-01-19 15:01:24 -0500

    issues getting high level protocols to work

--- src/main.c
+++ src/main.c
@@ -1,84 +1,76 @@
 #include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include <ctype.h>
 #include <ti/screen.h>
 #undef NDEBUG
 // #include <debug.h>
+
 #include <keypadc.h>
 #include <usbdrvce.h>
 #include "include/lwip/init.h"
 #include "include/lwip/netif.h"
+#include "include/lwip/tcp.h"
+#include "include/lwip/udp.h"
 #include "include/lwip/timeouts.h"
+#include "include/lwip/sys.h"
 #include "include/lwip/snmp.h"
+#include "include/lwip/pbuf.h"
 #include "include/lwip/dhcp.h"
-#include "include/lwip/apps/httpd.h"
-#include "include/lwip/ethip6.h"
 #include "ecm.h"
 
-#define MAX_DEViCES 10
-
-static void
-netif_status_callback(struct netif *netif)
-{
-    dbg_printf("netif status changed %s\n", ip4addr_ntoa(netif_ip4_addr(netif)));
-}
-
-static err_t
-my_netif_init(struct netif *netif)
-{
-    netif->linkoutput = ecm_transmit;
-    netif->output = etharp_output;
-    netif->output_ip6 = ethip6_output;
-    netif->mtu = ECM_MTU;
-    netif->flags = NETIF_FLAG_BROADCAST | NETIF_FLAG_ETHARP | NETIF_FLAG_ETHERNET | NETIF_FLAG_IGMP | NETIF_FLAG_MLD6;
-    MIB2_INIT_NETIF(netif, snmp_ifType_ethernet_csmacd, 100000000);
-    memcpy(netif->hwaddr, ecm_device.usb.mac_addr, ETH_HWADDR_LEN);
-    netif->hwaddr_len = ETH_HWADDR_LEN;
-    return ERR_OK;
-}
+struct udp_pcb *pcb;
 
 int main(void)
 {
-    os_ClrHomeFull();
+    os_ClrLCDFull();
     // initialize usb hardware
+    lwip_init();
+    pbuf_init();
+    udp_init();
+    pcb = udp_new();
     if (usb_Init(ecm_handle_usb_event, NULL, NULL /* descriptors */, USB_DEFAULT_INIT_FLAGS))
         return 1;
 
     // wait for ecm device to be ready
-    bool netif_init = false;
+    bool ip_setup_done = false;
+
     kb_SetMode(MODE_3_CONTINUOUS);
     do
     {
-        if (ecm_device.status == ECM_READY && netif_init == false)
+        if (kb_IsDown(kb_KeyAlpha))
         {
-            // queue ecm_receive
-            // configure ip addr info
-            printf("device ready\n");
-            lwip_init();
-            ip4_addr_t addr = IPADDR4_INIT_BYTES(192, 168, 25, 2);
-            ip4_addr_t netmask = IPADDR4_INIT_BYTES(255, 255, 255, 0);
-            ip4_addr_t gateway = IPADDR4_INIT_BYTES(192, 168, 1, 1);
-            netif_add(&ecm_device.netif, &addr, &netmask, &gateway, NULL, my_netif_init, netif_input);
-            ecm_device.netif.name[0] = 'e';
-            ecm_device.netif.name[1] = 'n';
-            ecm_device.netif.num = 0;
-            netif_create_ip6_linklocal_address(&ecm_device.netif, 1);
-            ecm_device.netif.ip6_autoconfig_enabled = 1;
-            netif_set_status_callback(&ecm_device.netif, netif_status_callback);
-            netif_set_default(&ecm_device.netif);
-            netif_set_up(&ecm_device.netif);
-            netif_init = true;
+            if (dhcp_supplied_address(&ecm_device.netif))
+                printf("dhcp_addr %s\n", ip4addr_ntoa(netif_ip4_addr(&ecm_device.netif)));
+            else
+                printf("no ip configd\n");
+        }
+        if (kb_IsDown(kb_Key2nd))
+        {
+            static const char *text1 = "The fox jumped over the dog.";
+            ip4_addr_t sendto = IPADDR4_INIT_BYTES(192, 168, 1, 216);
+            struct pbuf *p = pbuf_alloc(PBUF_TRANSPORT, strlen(text1), PBUF_RAM);
+            if (p)
+            {
+                pbuf_take(p, text1, strlen(text1));
+                printf("%i\n", udp_sendto(pcb, p, &sendto, 51000));
+            }
+            else
+            {
+                printf("buf alloc err\n");
+                pbuf_free(p);
+            }
         }
         if (kb_IsDown(kb_KeyClear))
             break;
+        sys_check_timeouts();
         usb_HandleEvents();
     } while (1);
-
+    dhcp_stop(&ecm_device.netif);
     usb_Cleanup();
     /* Start DHCP and HTTPD */
-    //    dhcp_start(&netif);
-    // httpd_init();
+    //
 
     // todo: actually check for this
     // netif_set_link_up(&netif);
