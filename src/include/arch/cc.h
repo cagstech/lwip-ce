3181be9f731cbce3dae7fb4095162d915045a352
Anthony Cagliano <acagliano97@gmail.com>
2024-01-20 11:26:27 -0500

    better debug out

--- src/include/arch/cc.h
+++ src/include/arch/cc.h
@@ -2,8 +2,17 @@
 #define LWIP_CC_H
 
 #undef NDEBUG
+#include <string.h>
+#include <sys/lcd.h>
+#include <stdio.h>
 #include <debug.h>
+#include <graphx.h>
 
-#define LWIP_PLATFORM_DIAG(x) do {dbg_printf x;} while(0)
+#define LWIP_PLATFORM_DIAG(x) \
+  do                          \
+  {                           \
+    printf x;                 \
+    putchar("\n");            \
+  } while (0)
 
-#endif //LWIP_CC_H
+#endif // LWIP_CC_H
