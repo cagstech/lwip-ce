916c9f8e1caa6669b384ee9302036c1ebb24cbb8
Anthony Cagliano <acagliano97@gmail.com>
2024-01-26 12:05:23 -0500

    dhcp works as of 11.2 build of toolchain

--- src/arch/sys_arch.c
+++ src/arch/sys_arch.c
@@ -1,19 +1,19 @@
 #include "lwip/arch.h"
+#include <sys/timers.h>
 #include <time.h>
+#include <usbdrvce.h>
 
-u32_t
-sys_jiffies(void)
+u32_t sys_jiffies(void)
 {
     return clock();
 }
 
-u32_t
-sys_now(void)
+u32_t sys_now(void)
 {
     return sys_jiffies() * 1000 / CLOCKS_PER_SEC;
 }
 
-void
-sys_init(void)
+void sys_init(void)
 {
+    timer_Enable(1, TIMER_32K, TIMER_NOINT, TIMER_UP);
 }
