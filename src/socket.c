a209a9646a4a7fc334183a3b82c9d1ac0366bd01
Anthony Cagliano <acagliano97@gmail.com>
2024-01-30 23:40:05 -0500

    recent changes to attempt to fix tcp test

--- src/socket.c
+++
@@ -1,9 +0,0 @@
-// socket API for TI-84+ CE
-// by Anthony Cagliano
-#include <stdbool.h>
-#include "ecm.h"
-#include "socket.h"
-
-bool subsys_init = false;
-
-bool socket_create(struct socket *sock, int domain, int type, int protocol) {}
