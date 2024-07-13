91564d5d86ebca4cf92d701948d02e80cc9d9228
Anthony Cagliano <admin@cagstech.com>
2024-07-13 11:18:53 -0700

    finish implementation of hash frontend

---
+++ src/tls/hashes/hashes.asm
@@ -0,0 +1,174 @@
+;--------------------------------------------
+; tls_Hash frontend EZ80 implementation
+; code by acagliano
+;--------------------------------------------
+; include to supported algorithms
+include "sha256.asm"
+
+;------------------------------------
+; Public Functions
+export _tls_hash_init
+export _tls_hash_update
+export _tls_hash_digest
+
+;-------------------------
+; call hl
+_indcallhl:
+; Calls HL
+; Inputs:
+;  HL : Address to call
+	jp	(hl)
+
+; ------------------------------------------------------
+; Define total size of hash context (including state)
+tls_hash_context_state_size := _sha256ctx_size  ; change this if the largest context changes
+tls_hash_total_size := tls_hash_context_state_size + tls_hash_context_header_size
+tls_hashes_implemented := 2
+
+;--------------------------------
+; Define hash header format
+virtual at 0
+	algorithmId     rb 1
+	digestLen       rb 1
+	tls_hash_context_header_size:
+end virtual
+
+;----------------------------------------------------------------------
+; struct tls_Hash *tls_hash_context_create(uint8_t algorithm_id);
+_tls_hash_context_create;
+; dynamically allocates a tls_Hash context for use with the Hash api.
+; in = algorithm_id
+; out = *tls_Hash
+	pop bc, hl
+	push hl, bc   ; hl has hash algorithm id
+	ld a, l       ; algorithm id into c
+
+	; check algorithm id valid
+	ld b, tls_hashes_implemented
+	cp a, b       ; a - b               ; check if a < b
+	jr c, .algorithmid_ok               ; if carry set, a < b, then goto ok
+	or	a,a
+	sbc	hl,hl
+	ret           ; return NULL;
+.algorithmid_ok:
+	ld b, 0     ; we can do this, push bc before _malloc, pop it after
+	ld c, a     ; save a in c (allows up to remove a few ld c, a instructions later)
+	; allocate context
+	ld hl, tls_hash_total_size
+	push bc
+	call _malloc    ; malloc the size of the hash header + state struct
+	pop bc
+	add hl, bc
+	or a
+	sbc hl, bc  ; if hl == 0
+	ret z       ; return NULL;
+	push hl
+	pop ix    ; hl into ix
+	ld (ix + 0), c   ; set ctx->algorithm_id = algorithm_id
+	ld l, c
+	ld h, 3
+	mlt hl
+	push hl
+	pop bc          ; bc should have correct offset
+	; set digest length for algorithm, bc saved earlier
+	ld hl, tls_hash_digest_len
+	add hl, bc  ; hl should now point to correct digest len
+	ld (ix + 1), (hl)   ; ctx->digest_len
+	; now we call to the hash init algorithm with ctx->state
+	pea ix
+	pea ix + tls_hash_context_header_size
+	ld hl, tls_hash_init_funcs
+	add hl, bc        ; get the correct hash init function
+	call _indcallhl   ; call it
+	pop de  ; we no longer need that
+	pop hl    ; that we need to return to the caller
+	ret
+
+;---------------------------------------------------------------------------------
+; bool tls_hash_update(struct tls_Hash *context, void *data, size_t len);
+_tls_hash_update:
+call  ti._frameset0
+; (ix+0) return vector
+; (ix+3) old ix
+; (ix+6) context
+; (ix+9) data
+; (ix+12) len
+	; validate algorithm_id again. users like to do all sorts of mess.
+	ld iy, (ix + 6)
+	ld a, (iy + 0)
+	ld b, tls_hashes_implemented
+	cp a, b       ; a - b               ; check if a < b
+	jr c, .algorithmid_ok               ; if carry set, a < b, then goto ok
+	xor a
+	jr .exit        ; return false;
+.algorithmid_ok:
+	; push ctx->state, data, len for inner call
+	ld hl, (ix + 12)
+	push hl
+	ld hl, (ix + 9)
+	push hl
+	pea iy + tls_hash_context_header_size   ; iy from earlier
+	ld hl, (iy + 3)
+	; get update func for hash
+	ld hl, tls_hash_update_funcs
+	ld b, 0
+	ld c, a
+	add hl, bc
+	call _indcallhl
+.exit:
+	ld sp, ix
+	pop ix
+	ret
+
+;---------------------------------------------------------------------------------
+; bool tls_hash_digest(struct tls_Hash *context, void *data, size_t len);
+_tls_hash_digest:
+call ti._frameset0
+; (ix+0) return vector
+; (ix+3) old ix
+; (ix+6) context
+; (ix+9) outbuf
+	ld hl, (ix + 9)
+	push hl
+	; validate algorithm_id again. users like to do all sorts of mess.
+	ld iy, (ix + 6)
+	ld a, (iy)
+	ld b, tls_hashes_implemented
+	cp a, b       ; a - b               ; check if a < b
+	jr c, .algorithmid_ok               ; if carry set, a < b, then goto ok
+	xor a
+	jr .exit        ; return false;
+.algorithm_ok:
+	pea iy + tls_hash_context_header_size
+	ld hl, (iy + 6)
+	call _indcallhl
+	ld sp, ix
+	pop ix
+	ret
+
+
+;-------------------------------------------------
+; Lookups for hash digest, functions
+
+; LUT for hash digest lengths
+tls_hash_digest_len:
+	db 20
+	db 32
+
+; LUT for hash init
+tls_hash_init_funcs:
+	dl hash_sha1_init
+	dl hash_sha256_init
+
+; LUT for hash update
+tls_hash_update_funcs:
+	dl hash_sha1_update
+	dl hash_sha256_update
+
+; LUT for hash digest
+tls_hash_digest_funcs:
+	dl hash_sha1_digest
+	dl hash_sha256_digest
+
+
+extern _malloc
--- src/tls/hashes/hashes.h
+++ src/tls/hashes/hashes.h
@@ -8,8 +8,7 @@
 #ifndef hashes_h
 #define hashes_h
 
-#include <stdio.h>
-#include "../helpers/tls_err.h"
+#include <stdint.h>
 
 struct tls_Hash {
   uint8_t algorithm_id;
@@ -19,10 +18,11 @@ struct tls_Hash {
 
 // avoiding static allocation for contexts in lwip where possible
 struct tls_Hash *tls_hash_context_create(uint8_t hash_id);
-tls_err_t tls_hash_context_update(struct tls_Hash *context, const void *data, size_t len);
-tls_err_t tls_hash_context_digest(struct tls_Hash *context, void *digest);
+bool tls_hash_update(struct tls_Hash *context, const void *data, size_t len);
+bool tls_hash_digest(struct tls_Hash *context, void *digest);
 
 
+// moving this to an hmac module
 struct tls_Hmac {
   uint8_t algorithm_id;
   size_t digest_len;
